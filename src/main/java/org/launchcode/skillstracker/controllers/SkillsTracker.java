68a34b5930120499eb81347c84cafd63adb82e84
Merve Dogan <139923400+MerveDOGAN901@users.noreply.github.com>
2023-10-11 15:41:03 -0500

    finish

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsTracker.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsTracker.java
@@ -5,19 +5,18 @@ package org.launchcode.skillstracker.controllers;
         import org.springframework.web.bind.annotation.PostMapping;
         import org.springframework.web.bind.annotation.RequestParam;
         import org.springframework.web.bind.annotation.ResponseBody;
-        import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody
 public class SkillsTracker {
 
     // Handler that corresponds to localhost:8080
 
-    @GetMapping()
-    @ResponseBody
+    @GetMapping("/")
     public String displayHomePage() {
         return "<html>" +
                 "<body>" +
-                "<h1>Skills Trackers</h1>" +
+                "<h1>Skills Tracker</h1>" +
                 "<h2>We have a few skills we would like to learn. Here is the list!</h2>" +
                 "<ol>" +
                 "<li>Java</li>" +
@@ -32,12 +31,11 @@ public class SkillsTracker {
     }
 
     // Handler that corresponds to localhost:8080/form to display form
-    @GetMapping("form")
-    @ResponseBody
+    @GetMapping("/form")
     public String displaySkillsForm() {
         return "<html>" +
                 "<body>" +
-                "<form action = 'form' method = 'POST'>" +
+                "<form action = '/results' method = 'POST'>" +
                 "<label>Name:</label>" +
                 "<input type ='text' name= 'username' /><br />" +
                 "<label>My favorite programming language:</label>" +
@@ -47,7 +45,7 @@ public class SkillsTracker {
                 "<option value = 'TypeScript'> TypeScript </option>" +
                 "<option value = 'Python'> Python </option>" +
                 "<option value = 'Ruby'> Ruby </option>" +
-                "</select>" +
+                "</select><br/>" +
                 "<label>My second favorite programming language:</label>" +
                 "<select name='fave2'>" +
                 "<option value = 'Java'> Java </option>" +
@@ -55,7 +53,7 @@ public class SkillsTracker {
                 "<option value = 'TypeScript'> TypeScript </option>" +
                 "<option value = 'Python'> Python </option>" +
                 "<option value = 'Ruby'> Ruby </option>" +
-                "</select>" +
+                "</select><br/>"+
                 "<label>My third favorite programming language:</label>" +
                 "<select name='fave3'>" +
                 "<option value = 'Java'> Java </option>" +
@@ -63,7 +61,7 @@ public class SkillsTracker {
                 "<option value = 'TypeScript'> TypeScript </option>" +
                 "<option value = 'Python'> Python </option>" +
                 "<option value = 'Ruby'> Ruby </option>" +
-                "</select>" +
+                "</select><br/>" +
                 "<br>" +
                 "<input type = 'submit' value = 'Submit'/>" +
                 "</form>" +
@@ -72,12 +70,13 @@ public class SkillsTracker {
     }
 
     // Handler that corresponds to localhost:8080/form to display form AND display results
-    @PostMapping("form")
-    @ResponseBody
-    public String processSkillsForm(@RequestParam String userName, String fave1, String fave2, String fave3) {
+    @PostMapping("/results")
+    public String processSkillsForm(
+            @RequestParam String username, String fave1, String fave2, String fave3){
         return "<html>" +
                 "<body>" +
-                "<h1>" + userName + "</h1>" +
+                "<h1>" + username + "</h1>" +
+                "<h3>Favorite Languages</h3>"+
                 "<table>" +
                 "<tr>"+
                 "<td>1.<td>"+
@@ -85,11 +84,11 @@ public class SkillsTracker {
                 "</tr>" +
                 "<tr>" +
                 "<td>2.<td>" +
-                "<td>" + fave1 + "</td>" +
+                "<td>" + fave2 + "</td>" +
                 "</tr>" +
                 "<tr>" +
                 "<td>3.<td>"+
-                "<td>" + fave1 + "</td>" +
+                "<td>" + fave3 + "</td>" +
                 "</tr>" +
                 "</table>" +
                 "</body>" +
